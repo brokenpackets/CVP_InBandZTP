fe1b25d0e687db65d3a6c71883de9bfe6122d74a
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2020-08-10 14:35:33 -0700

    Create BLDR_ZTP_L2Domain.py

---
+++ BLDR_ZTP_L2Domain.py
@@ -0,0 +1,17 @@
+from cvplibrary import CVPGlobalVariables, GlobalVariableNames, Device
+
+Vlan_Id = 1
+
+# Check if Device is in ZTP Mode:
+if CVPGlobalVariables.getValue(GlobalVariableNames.ZTP_STATE) == 'true':
+    device_ip = CVPGlobalVariables.getValue(GlobalVariableNames.CVP_IP)
+    device_user = CVPGlobalVariables.getValue(GlobalVariableNames.ZTP_USERNAME)
+    device_pass =CVPGlobalVariables.getValue(GlobalVariableNames.ZTP_PASSWORD)
+    device = Device(device_ip,device_user,device_pass)
+    iflist = device.runCmds(['enable','show interfaces status'])[1]['response']
+    for item in iflist['interfaceStatuses'].keys():
+       if item.startswith('Ethernet'):
+           device.runCmds(['enable','configure','interface '+item,'switchport','switchport mode access','switchport access vlan '+str(Vlan_Id)])
+else:
+    pass
+#end device set-up.
