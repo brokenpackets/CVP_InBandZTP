06d808918af901bea6286a09585a7e2116fd452d
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2020-08-11 16:59:09 -0700

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -2,7 +2,7 @@
 import requests
 import json
 import yaml
-
+import os
 ###### User Variables
 
 username = 'admin'
@@ -10,7 +10,6 @@ password = 'Arista123'
 server1 = 'https://192.168.255.50'
 imageName = 'EOS-4.23.4M.swi'
 terminAttr = 'TerminAttr-1.9.6-1.swix'
-base_container_name = 'Tenant'
 containers_To_Build = ['Leaf', 'Spine', 'MGMT-ToR', 'MGMT-Spine']
 
 ###### Do not modify anything below this line. Or do, I'm not a cop.
@@ -75,40 +74,31 @@ def add_Container(url_prefix, container_name):
     print json.dumps(data)
     return response.json()
 
-def add_temp_action(url_prefix,container_name,container_key,current_static_key,
-          current_static_name,current_builder_key,current_builder_name):
+def add_configlet(url_prefix,configlet_name,configlet_body):
   tempData = json.dumps({
-    "data":[
-      {
-         "info":"Configlet Assign: to container "+container_name,
-         "infoPreview":"<b>Configlet Assign:</b> to container "+container_name,
-         "action":"associate",
-         "nodeType":"configlet",
-         "nodeId":"",
-         "toId":container_key,
-         "fromId":"",
-         "nodeName":"",
-         "fromName":"",
-         "toName":container_name,
-         "toIdType":"container",
-         "configletList":current_static_key,
-         "configletNamesList":current_static_name,
-         "ignoreConfigletList":[],
-         "ignoreConfigletNamesList":[],
-         "configletBuilderList":current_builder_key,
-         "configletBuilderNamesList":current_builder_name,
-         "ignoreConfigletBuilderList":[],
-         "ignoreConfigletBuilderNamesList":[]
-      }
-   ]
-})
-
-  response = session.post(url_prefix+'/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root', data=tempData)
+          "config": configlet_body,
+          "name": configlet_name
+  })
+  response = session.post(url_prefix+'/cvpservice/configlet/addConfiglet.do', data=tempData)
   #return tempData
   return response.json()
 
+def add_configlet_builder(url_prefix,builder_name,builder_body):
+    tempData = json.dumps({
+          "name": builder_name,
+          "data": {
+              "main_script": {
+                  "data": builder_body
+              }
+          }
+    })
+    response = session.post(url_prefix+'/cvpservice/configlet/addConfigletBuilder.do?isDraft=false', data=tempData)
+    return response.json()
+
+#### Login ####
 print '###### Logging into Server 1'
 login(server1, username, password)
+#### Upload Image Bundle SWI File ####
 print '###### Uploading Image Bundle'
 try:
     bundleinfo = upload_image(server1,imageName)
@@ -116,6 +106,7 @@ except:
     print 'Failure to upload file. Does it exist already? Is it in the working dir?'
     pass
 bundleinfo.pop('result',None)
+#### Upload Image Bundle TerminAttr SWIX ####
 try:
     terminAttrinfo = upload_image(server1,terminAttr)
 except:
@@ -123,13 +114,26 @@ except:
     pass
 bundleinfo.pop('result',None)
 terminAttrinfo.pop('result',None)
+#### Create Image Bundle with both SWI and SWIX ####
 try:
     add_Bundle(server1,json.dumps(bundleinfo,terminAttrinfo))
 except:
     print 'Add bundle failed for some reason...'
     pass
+#### Create Container Structure ####
 for container in containers_To_Build:
     try:
         add_Container(server1,container)
     except:
         pass
+#### Upload Configlets / Builders ####
+configlets = os.listdir('./Configlets')
+for configlet in configlets:
+    if configlet.endswith('.py'):
+        with open('./Configlets/'+configlet,'r') as f:
+            output = add_configlet_builder(server1,configlet,f.read())
+            print output
+    else:
+        with open('./Configlets/'+configlet,'r') as f:
+            output = add_configlet(server1,configlet,f.read())
+            print output
