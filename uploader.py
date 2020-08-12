1fadfd255bf9a7f116dc8eacbe5b01bde25b860d
Eric Tedor <et@arista.com>
2020-08-12 12:47:44 -0700

    2to3, pipenv init, format w/black, add .gitignore

--- uploader.py
+++ uploader.py
@@ -3,137 +3,167 @@ import requests
 import json
 import yaml
 import os
+
 ###### User Variables
 
-username = 'admin'
-password = 'Arista123'
-server1 = 'https://192.168.255.50'
-imageName = 'EOS-4.23.4M.swi'
-terminAttr = 'TerminAttr-1.9.6-1.swix'
-containers_To_Build = ['Leaf', 'Spine', 'MGMT-ToR', 'MGMT-Spine']
+username = "admin"
+password = "Arista123"
+server1 = "https://192.168.255.50"
+imageName = "EOS-4.23.4M.swi"
+terminAttr = "TerminAttr-1.9.6-1.swix"
+containers_To_Build = ["Leaf", "Spine", "MGMT-ToR", "MGMT-Spine"]
 
 ###### Do not modify anything below this line. Or do, I'm not a cop.
 connect_timeout = 10
-headers = {"Accept": "application/json",
-           "Content-Type": "application/json"}
+headers = {"Accept": "application/json", "Content-Type": "application/json"}
 requests.packages.urllib3.disable_warnings()
 session = requests.Session()
 
+
 def login(url_prefix, username, password):
     authdata = {"userId": username, "password": password}
-    headers.pop('APP_SESSION_ID', None)
-    response = session.post(url_prefix+'/web/login/authenticate.do', data=json.dumps(authdata),
-                            headers=headers, timeout=connect_timeout,
-                            verify=False)
+    headers.pop("APP_SESSION_ID", None)
+    response = session.post(
+        url_prefix + "/web/login/authenticate.do",
+        data=json.dumps(authdata),
+        headers=headers,
+        timeout=connect_timeout,
+        verify=False,
+    )
     cookies = response.cookies
-    headers['APP_SESSION_ID'] = response.json()['sessionId']
-    if response.json()['sessionId']:
-        return response.json()['sessionId']
+    headers["APP_SESSION_ID"] = response.json()["sessionId"]
+    if response.json()["sessionId"]:
+        return response.json()["sessionId"]
+
 
 def logout(url_prefix):
-    response = session.post(url_prefix+'/web/login/logout.do')
+    response = session.post(url_prefix + "/web/login/logout.do")
     return response.json()
+
 
 def save_topology(url_prefix):
-    response = session.post(url_prefix+'/cvpservice/provisioning/v2/saveTopology.do', data=json.dumps([]))
+    response = session.post(
+        url_prefix + "/cvpservice/provisioning/v2/saveTopology.do", data=json.dumps([])
+    )
     return response.json()
 
-def add_configlet(url_prefix,configlet_name,configlet_body):
-  tempData = json.dumps({
-          "config": configlet_body,
-          "name": configlet_name
-  })
-  response = session.post(url_prefix+'/cvpservice/configlet/addConfiglet.do', data=tempData)
-  #return tempData
-  return response.json()
 
-def upload_image(url_prefix,imageName):
+def add_configlet(url_prefix, configlet_name, configlet_body):
+    tempData = json.dumps({"config": configlet_body, "name": configlet_name})
+    response = session.post(
+        url_prefix + "/cvpservice/configlet/addConfiglet.do", data=tempData
+    )
+    # return tempData
+    return response.json()
+
+
+def upload_image(url_prefix, imageName):
     with open(imageName, "rb") as imageBinary:
-        imageDict = {'file': imageBinary}
-        response = session.post(url_prefix+'/cvpservice/image/addImage.do', files=imageDict)
+        imageDict = {"file": imageBinary}
+        response = session.post(
+            url_prefix + "/cvpservice/image/addImage.do", files=imageDict
+        )
     return response.json()
 
-def add_Bundle(url_prefix,imageBundleName):
-    data = {"images": [bundleinfo, terminAttrinfo], "isCertifiedImage": 'true', "name": 'DefaultBundle'}
-    response = session.post(url_prefix+'/cvpservice/image/saveImageBundle.do', data=json.dumps(data))
+
+def add_Bundle(url_prefix, imageBundleName):
+    data = {
+        "images": [bundleinfo, terminAttrinfo],
+        "isCertifiedImage": "true",
+        "name": "DefaultBundle",
+    }
+    response = session.post(
+        url_prefix + "/cvpservice/image/saveImageBundle.do", data=json.dumps(data)
+    )
     return response.json()
+
 
 def add_Container(url_prefix, container_name):
-    data = {'data': [{'info': 'adding Container '+container_name,
-                          'infoPreview': container_name,
-                          'action': 'add',
-                          'nodeType': 'container',
-                          'nodeId': container_name,
-                          'toId': 'root',
-                          'fromId': '',
-                          'nodeName': container_name,
-                          'fromName': '',
-                          'toName': 'Tenant',
-                          'toIdType': 'container'}]}
-    response = session.post(url_prefix+'/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root', data=json.dumps(data))
-    print json.dumps(data)
+    data = {
+        "data": [
+            {
+                "info": "adding Container " + container_name,
+                "infoPreview": container_name,
+                "action": "add",
+                "nodeType": "container",
+                "nodeId": container_name,
+                "toId": "root",
+                "fromId": "",
+                "nodeName": container_name,
+                "fromName": "",
+                "toName": "Tenant",
+                "toIdType": "container",
+            }
+        ]
+    }
+    response = session.post(
+        url_prefix
+        + "/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root",
+        data=json.dumps(data),
+    )
+    print(json.dumps(data))
     return response.json()
 
-def add_configlet(url_prefix,configlet_name,configlet_body):
-  tempData = json.dumps({
-          "config": configlet_body,
-          "name": configlet_name
-  })
-  response = session.post(url_prefix+'/cvpservice/configlet/addConfiglet.do', data=tempData)
-  #return tempData
-  return response.json()
 
-def add_configlet_builder(url_prefix,builder_name,builder_body):
-    tempData = json.dumps({
-          "name": builder_name,
-          "data": {
-              "main_script": {
-                  "data": builder_body
-              }
-          }
-    })
-    response = session.post(url_prefix+'/cvpservice/configlet/addConfigletBuilder.do?isDraft=false', data=tempData)
+def add_configlet(url_prefix, configlet_name, configlet_body):
+    tempData = json.dumps({"config": configlet_body, "name": configlet_name})
+    response = session.post(
+        url_prefix + "/cvpservice/configlet/addConfiglet.do", data=tempData
+    )
+    # return tempData
     return response.json()
+
+
+def add_configlet_builder(url_prefix, builder_name, builder_body):
+    tempData = json.dumps(
+        {"name": builder_name, "data": {"main_script": {"data": builder_body}}}
+    )
+    response = session.post(
+        url_prefix + "/cvpservice/configlet/addConfigletBuilder.do?isDraft=false",
+        data=tempData,
+    )
+    return response.json()
+
 
 #### Login ####
-print '###### Logging into Server 1'
+print("###### Logging into Server 1")
 login(server1, username, password)
 #### Upload Image Bundle SWI File ####
-print '###### Uploading Image Bundle'
+print("###### Uploading Image Bundle")
 try:
-    bundleinfo = upload_image(server1,imageName)
+    bundleinfo = upload_image(server1, imageName)
 except:
-    print 'Failure to upload file. Does it exist already? Is it in the working dir?'
+    print("Failure to upload file. Does it exist already? Is it in the working dir?")
     pass
-bundleinfo.pop('result',None)
+bundleinfo.pop("result", None)
 #### Upload Image Bundle TerminAttr SWIX ####
 try:
-    terminAttrinfo = upload_image(server1,terminAttr)
+    terminAttrinfo = upload_image(server1, terminAttr)
 except:
-    print 'Failure to upload file. Does it exist already? Is it in the working dir?'
+    print("Failure to upload file. Does it exist already? Is it in the working dir?")
     pass
-bundleinfo.pop('result',None)
-terminAttrinfo.pop('result',None)
+bundleinfo.pop("result", None)
+terminAttrinfo.pop("result", None)
 #### Create Image Bundle with both SWI and SWIX ####
 try:
-    add_Bundle(server1,json.dumps(bundleinfo,terminAttrinfo))
+    add_Bundle(server1, json.dumps(bundleinfo, terminAttrinfo))
 except:
-    print 'Add bundle failed for some reason...'
+    print("Add bundle failed for some reason...")
     pass
 #### Create Container Structure ####
 for container in containers_To_Build:
     try:
-        add_Container(server1,container)
+        add_Container(server1, container)
     except:
         pass
 #### Upload Configlets / Builders ####
-configlets = os.listdir('./Configlets')
+configlets = os.listdir("./Configlets")
 for configlet in configlets:
-    if configlet.endswith('.py'):
-        with open('./Configlets/'+configlet,'r') as f:
-            output = add_configlet_builder(server1,configlet,f.read())
-            print output
+    if configlet.endswith(".py"):
+        with open("./Configlets/" + configlet, "r") as f:
+            output = add_configlet_builder(server1, configlet, f.read())
+            print(output)
     else:
-        with open('./Configlets/'+configlet,'r') as f:
-            output = add_configlet(server1,configlet,f.read())
-            print output
+        with open("./Configlets/" + configlet, "r") as f:
+            output = add_configlet(server1, configlet, f.read())
+            print(output)
--- ztp_AutoProvision.py
+++ ztp_AutoProvision.py
@@ -5,207 +5,300 @@ import yaml
 
 ###### User Variables
 
-username = 'admin'
-password = 'Arista123'
-server1 = 'https://192.168.255.50'
-container_name = 'Tenant'
-yaml_name = 'ztp_SeedData.yaml'
+username = "admin"
+password = "Arista123"
+server1 = "https://192.168.255.50"
+container_name = "Tenant"
+yaml_name = "ztp_SeedData.yaml"
 
 ###### Do not modify anything below this line. Or do, I'm not a cop.
 connect_timeout = 10
-headers = {"Accept": "application/json",
-           "Content-Type": "application/json"}
+headers = {"Accept": "application/json", "Content-Type": "application/json"}
 requests.packages.urllib3.disable_warnings()
 session = requests.Session()
 
+
 def login(url_prefix, username, password):
     authdata = {"userId": username, "password": password}
-    headers.pop('APP_SESSION_ID', None)
-    response = session.post(url_prefix+'/web/login/authenticate.do', data=json.dumps(authdata),
-                            headers=headers, timeout=connect_timeout,
-                            verify=False)
+    headers.pop("APP_SESSION_ID", None)
+    response = session.post(
+        url_prefix + "/web/login/authenticate.do",
+        data=json.dumps(authdata),
+        headers=headers,
+        timeout=connect_timeout,
+        verify=False,
+    )
     cookies = response.cookies
-    headers['APP_SESSION_ID'] = response.json()['sessionId']
-    if response.json()['sessionId']:
-        return response.json()['sessionId']
+    headers["APP_SESSION_ID"] = response.json()["sessionId"]
+    if response.json()["sessionId"]:
+        return response.json()["sessionId"]
+
 
 def logout(url_prefix):
-    response = session.post(url_prefix+'/web/login/logout.do')
+    response = session.post(url_prefix + "/web/login/logout.do")
     return response.json()
 
+
 def get_inventory(url_prefix):
-    response = session.get(url_prefix+'/cvpservice/inventory/devices')
+    response = session.get(url_prefix + "/cvpservice/inventory/devices")
     if response.json():
         return response.json()
 
-def get_builder(url_prefix,builder_name):
-    response = session.get(url_prefix+'/cvpservice/configlet/getConfigletByName.do?name='+builder_name)
-    if response.json()['key']:
-        return response.json()['key']
 
-def get_container_configlets(url_prefix,container_key):
-    response = session.get(url_prefix+'/cvpservice/ztp/getTempConfigsByContainerId.do?containerId='+container_key)
+def get_builder(url_prefix, builder_name):
+    response = session.get(
+        url_prefix + "/cvpservice/configlet/getConfigletByName.do?name=" + builder_name
+    )
+    if response.json()["key"]:
+        return response.json()["key"]
+
+
+def get_container_configlets(url_prefix, container_key):
+    response = session.get(
+        url_prefix
+        + "/cvpservice/ztp/getTempConfigsByContainerId.do?containerId="
+        + container_key
+    )
     return response.json()
 
-def get_configlets_by_device(url_prefix,deviceMac):
-    response = session.get(url_prefix+'/cvpservice/provisioning/getConfigletsByNetElementId.do?netElementId='+deviceMac+'&startIndex=0&endIndex=0')
+
+def get_configlets_by_device(url_prefix, deviceMac):
+    response = session.get(
+        url_prefix
+        + "/cvpservice/provisioning/getConfigletsByNetElementId.do?netElementId="
+        + deviceMac
+        + "&startIndex=0&endIndex=0"
+    )
     return response.json()
 
-def get_configlet_by_name(url_prefix,configletname):
-    response = session.get(url_prefix+'/cvpservice/configlet/getConfigletByName.do?name='+configletname)
+
+def get_configlet_by_name(url_prefix, configletname):
+    response = session.get(
+        url_prefix + "/cvpservice/configlet/getConfigletByName.do?name=" + configletname
+    )
     return response.json()
 
-def search_configlets(url_prefix,configletname):
-    response = session.get(url_prefix+'/cvpservice/configlet/searchConfiglets.do?type=static&queryparam='+configletname+'&startIndex=0&endIndex=0')
+
+def search_configlets(url_prefix, configletname):
+    response = session.get(
+        url_prefix
+        + "/cvpservice/configlet/searchConfiglets.do?type=static&queryparam="
+        + configletname
+        + "&startIndex=0&endIndex=0"
+    )
     return response.json()
 
-def get_container(url_prefix,container_name):
-    response = session.get(url_prefix+'/cvpservice/provisioning/searchTopology.do?queryParam='+container_name+'&startIndex=0&endIndex=0')
-    if response.json()['containerList'][0]['key']:
-      return response.json()['containerList'][0]['key']
 
-def get_temp_configs(url_prefix,nodeId):
-    response = session.get(url_prefix+'/cvpservice/provisioning/getTempConfigsByNetElementId.'
-                                      'do?netElementId='+nodeId)
+def get_container(url_prefix, container_name):
+    response = session.get(
+        url_prefix
+        + "/cvpservice/provisioning/searchTopology.do?queryParam="
+        + container_name
+        + "&startIndex=0&endIndex=0"
+    )
+    if response.json()["containerList"][0]["key"]:
+        return response.json()["containerList"][0]["key"]
+
+
+def get_temp_configs(url_prefix, nodeId):
+    response = session.get(
+        url_prefix + "/cvpservice/provisioning/getTempConfigsByNetElementId."
+        "do?netElementId=" + nodeId
+    )
     return response.json()
 
-def run_builder(url_prefix,configlet_key,container_key):
-    data = json.dumps({"netElementIds":[],"configletBuilderId":configlet_key,"containerId":container_key,"pageType":"container"})
-    response = session.post(url_prefix+'/cvpservice/configlet/autoConfigletGenerator.do', data=data)
+
+def run_builder(url_prefix, configlet_key, container_key):
+    data = json.dumps(
+        {
+            "netElementIds": [],
+            "configletBuilderId": configlet_key,
+            "containerId": container_key,
+            "pageType": "container",
+        }
+    )
+    response = session.post(
+        url_prefix + "/cvpservice/configlet/autoConfigletGenerator.do", data=data
+    )
     return response.json()
+
 
 def save_topology(url_prefix):
-    response = session.post(url_prefix+'/cvpservice/provisioning/v2/saveTopology.do', data=json.dumps([]))
+    response = session.post(
+        url_prefix + "/cvpservice/provisioning/v2/saveTopology.do", data=json.dumps([])
+    )
     return response.json()
 
-def apply_configlets(url_prefix,nodeName,nodeIp,deviceMac,newConfiglets):
+
+def apply_configlets(url_prefix, nodeName, nodeIp, deviceMac, newConfiglets):
     configlets = get_configlets_by_device(url_prefix, deviceMac)
     cnames = []
     ckeys = []
 
     # Add the new configlets to the end of the arrays
     for entry in newConfiglets:
-        cnames.append(entry['name'])
-        ckeys.append(entry['key'])
+        cnames.append(entry["name"])
+        ckeys.append(entry["key"])
 
-    info = 'ZTPBuilder: Configlet Assign: to Device '+nodeName
-    info_preview = '<b>Configlet Assign:</b> to Device '+nodeName
-    tempData = json.dumps({
-        'data': [{'info': info,
-                      'infoPreview': info_preview,
-                      'note': '',
-                      'action': 'associate',
-                      'nodeType': 'configlet',
-                      'nodeId': '',
-                      'configletList': ckeys,
-                      'configletNamesList': cnames,
-                      'ignoreConfigletNamesList': [],
-                      'ignoreConfigletList': [],
-                      'configletBuilderList': [],
-                      'configletBuilderNamesList': [],
-                      'ignoreConfigletBuilderList': [],
-                      'ignoreConfigletBuilderNamesList': [],
-                      'toId': deviceMac,
-                      'toIdType': 'netelement',
-                      'fromId': '',
-                      'nodeName': '',
-                      'fromName': '',
-                      'toName': nodeName,
-                      'nodeIpAddress': nodeIp,
-                      'nodeTargetIpAddress': nodeIp,
-                      'childTasks': [],
-                      'parentTask': ''}]})
+    info = "ZTPBuilder: Configlet Assign: to Device " + nodeName
+    info_preview = "<b>Configlet Assign:</b> to Device " + nodeName
+    tempData = json.dumps(
+        {
+            "data": [
+                {
+                    "info": info,
+                    "infoPreview": info_preview,
+                    "note": "",
+                    "action": "associate",
+                    "nodeType": "configlet",
+                    "nodeId": "",
+                    "configletList": ckeys,
+                    "configletNamesList": cnames,
+                    "ignoreConfigletNamesList": [],
+                    "ignoreConfigletList": [],
+                    "configletBuilderList": [],
+                    "configletBuilderNamesList": [],
+                    "ignoreConfigletBuilderList": [],
+                    "ignoreConfigletBuilderNamesList": [],
+                    "toId": deviceMac,
+                    "toIdType": "netelement",
+                    "fromId": "",
+                    "nodeName": "",
+                    "fromName": "",
+                    "toName": nodeName,
+                    "nodeIpAddress": nodeIp,
+                    "nodeTargetIpAddress": nodeIp,
+                    "childTasks": [],
+                    "parentTask": "",
+                }
+            ]
+        }
+    )
 
-    response = session.post(url_prefix+'/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root', data=tempData)
-    #return tempData
+    response = session.post(
+        url_prefix
+        + "/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root",
+        data=tempData,
+    )
+    # return tempData
     return response.json()
 
 
-def move_device(url_prefix,nodeName,nodeId,toId,toName):
-  tempData = json.dumps({
-    "data": [
+def move_device(url_prefix, nodeName, nodeId, toId, toName):
+    tempData = json.dumps(
         {
-          "info": "Device "+toName+"move from undefined to Container "+toId,
-          "infoPreview": "<b>Device ZTP Add:</b> "+toName,
-          "action": "update",
-          "nodeType": "netelement",
-          "nodeId": nodeId,
-          "toId": toId,
-          "fromId": "undefined_container",
-          "nodeName": nodeName,
-          "toName": toName,
-          "toIdType": "container" }]
-  })
-  response = session.post(url_prefix+'/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root', data=tempData)
-  #return tempData
-  return response.json()
+            "data": [
+                {
+                    "info": "Device "
+                    + toName
+                    + "move from undefined to Container "
+                    + toId,
+                    "infoPreview": "<b>Device ZTP Add:</b> " + toName,
+                    "action": "update",
+                    "nodeType": "netelement",
+                    "nodeId": nodeId,
+                    "toId": toId,
+                    "fromId": "undefined_container",
+                    "nodeName": nodeName,
+                    "toName": toName,
+                    "toIdType": "container",
+                }
+            ]
+        }
+    )
+    response = session.post(
+        url_prefix
+        + "/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root",
+        data=tempData,
+    )
+    # return tempData
+    return response.json()
 
-def add_temp_action(url_prefix,container_name,container_key,current_static_key,
-          current_static_name,current_builder_key,current_builder_name):
-  tempData = json.dumps({
-    "data":[
-      {
-         "info":"Configlet Assign: to container "+container_name,
-         "infoPreview":"<b>Configlet Assign:</b> to container "+container_name,
-         "action":"associate",
-         "nodeType":"configlet",
-         "nodeId":"",
-         "toId":container_key,
-         "fromId":"",
-         "nodeName":"",
-         "fromName":"",
-         "toName":container_name,
-         "toIdType":"container",
-         "configletList":current_static_key,
-         "configletNamesList":current_static_name,
-         "ignoreConfigletList":[],
-         "ignoreConfigletNamesList":[],
-         "configletBuilderList":current_builder_key,
-         "configletBuilderNamesList":current_builder_name,
-         "ignoreConfigletBuilderList":[],
-         "ignoreConfigletBuilderNamesList":[]
-      }
-   ]
-})
 
-  response = session.post(url_prefix+'/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root', data=tempData)
-  #return tempData
-  return response.json()
+def add_temp_action(
+    url_prefix,
+    container_name,
+    container_key,
+    current_static_key,
+    current_static_name,
+    current_builder_key,
+    current_builder_name,
+):
+    tempData = json.dumps(
+        {
+            "data": [
+                {
+                    "info": "Configlet Assign: to container " + container_name,
+                    "infoPreview": "<b>Configlet Assign:</b> to container "
+                    + container_name,
+                    "action": "associate",
+                    "nodeType": "configlet",
+                    "nodeId": "",
+                    "toId": container_key,
+                    "fromId": "",
+                    "nodeName": "",
+                    "fromName": "",
+                    "toName": container_name,
+                    "toIdType": "container",
+                    "configletList": current_static_key,
+                    "configletNamesList": current_static_name,
+                    "ignoreConfigletList": [],
+                    "ignoreConfigletNamesList": [],
+                    "configletBuilderList": current_builder_key,
+                    "configletBuilderNamesList": current_builder_name,
+                    "ignoreConfigletBuilderList": [],
+                    "ignoreConfigletBuilderNamesList": [],
+                }
+            ]
+        }
+    )
 
-print '###### Logging into Server 1'
+    response = session.post(
+        url_prefix
+        + "/cvpservice/ztp/addTempAction.do?format=topology&queryParam=&nodeId=root",
+        data=tempData,
+    )
+    # return tempData
+    return response.json()
+
+
+print("###### Logging into Server 1")
 login(server1, username, password)
-print '###### Pulling down YAML File'
-yamlfile = get_configlet_by_name(server1,yaml_name)['config']
+print("###### Pulling down YAML File")
+yamlfile = get_configlet_by_name(server1, yaml_name)["config"]
 yamlbody = yaml.load(yamlfile)
-print '###### Getting list of devices in Undefined Container'
+print("###### Getting list of devices in Undefined Container")
 ztpdevices = get_inventory(server1)
 for device in ztpdevices:
-    if device['parentContainerKey'] == 'undefined_container':
+    if device["parentContainerKey"] == "undefined_container":
         for template in yamlbody:
-            if yamlbody[template]['Serial'] == device['serialNumber']:
-                nodeName = device['fqdn']
-                nodeId = device['systemMacAddress']
-                toId = get_container(server1,yamlbody[template]['container'])
+            if yamlbody[template]["Serial"] == device["serialNumber"]:
+                nodeName = device["fqdn"]
+                nodeId = device["systemMacAddress"]
+                toId = get_container(server1, yamlbody[template]["container"])
                 toName = template
-                nodeIp = device['ipAddress']
-                move = move_device(server1,nodeName,nodeId,toId,toName)
-                ds_configlets = search_configlets(server1,'DS_'+template+'_')
-                print ds_configlets
-                #configletList = get_configlets_by_device(server1,nodeId)
-                tempConfiglets = get_temp_configs(server1,nodeId)
-                newConfiglets = tempConfiglets['proposedConfiglets']
+                nodeIp = device["ipAddress"]
+                move = move_device(server1, nodeName, nodeId, toId, toName)
+                ds_configlets = search_configlets(server1, "DS_" + template + "_")
+                print(ds_configlets)
+                # configletList = get_configlets_by_device(server1,nodeId)
+                tempConfiglets = get_temp_configs(server1, nodeId)
+                newConfiglets = tempConfiglets["proposedConfiglets"]
                 dsList = []
-                if int(ds_configlets['total']) > 0:
-                  for config in ds_configlets['data']:
-                    output = get_configlet_by_name(server1,config['name'])
-                    dsList.extend([output])
-                  newConfiglets.extend(dsList)
-                  print 'Assigning DS Configlets to '+nodeName
-                  assign = apply_configlets(server1,nodeName,nodeIp,nodeId,newConfiglets)
+                if int(ds_configlets["total"]) > 0:
+                    for config in ds_configlets["data"]:
+                        output = get_configlet_by_name(server1, config["name"])
+                        dsList.extend([output])
+                    newConfiglets.extend(dsList)
+                    print("Assigning DS Configlets to " + nodeName)
+                    assign = apply_configlets(
+                        server1, nodeName, nodeIp, nodeId, newConfiglets
+                    )
 # Once temp action created, save will cause it to be committed, and generate
 # the tasks to run against devices. Can automate running them if needed, but
 # I generally prefer manual runs to validate it did what I expected.
-print '##### Saving Topology'
-#save = save_topology(server1)
-#logout(server1)
-print '##### Complete'
+print("##### Saving Topology")
+# save = save_topology(server1)
+# logout(server1)
+print("##### Complete")
+
